4d1e50d90a60e9c34794363db229a70eb214734f
Gabriel <gabriel.lazari@outlook.com>
2021-09-12 09:23:34 -0300

    product details

--- loja/settings.py
+++ loja/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from django.contrib.messages import constants
+# from dotenv import load_dotenv
+from decouple import config
+# import os
+
+# load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g&d_bdhr5dn7wf^*(9e8t(hscnez(^=c_h1l679=k!+md518nb'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
